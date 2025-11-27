0fe05f6d072e7eab7a4a0b36a6e9042d2ad90754
webforworld2512 <surabhichavan2512@gmail.com>
2025-11-27 13:00:18 -0800

    Webpack error fix

--- src/components/MeetingModal.tsx
+++ src/components/MeetingModal.tsx
@@ -29,7 +29,7 @@ const MeetingModal = ({ isOpen, onClose, title, className, handleClick, children
             {/* <DialogTrigger>Open</DialogTrigger> */}
             <DialogContent className='flex w-full max-w-[520px]
             flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white'>
-                <div className='flex-flex-col gap-6'>
+                <div className='flex flex-col gap-6'>
                     {image && (
                         <div className='flex justify-center'>
                             <Image src={image} alt="image" width={72} height={72} />
