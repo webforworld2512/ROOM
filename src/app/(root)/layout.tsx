f3b3cafb15e2f02651f229370ebd63c4d6b61abe
webforworld2512 <surabhichavan2512@gmail.com>
2025-11-27 12:15:04 -0800

    updated github package

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import React, { Children, ReactNode } from 'react'
+import StreamVideoProvider from '../../../providers/StreamClientProvider'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "ROOM",
+  description: "Video Calling App",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
+    </main>
+  )
+}
+
+export default RootLayout
---
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import MeetingRoom from '@/components/MeetingRoom';
+import MeetingSetup from '@/components/MeetingSetup';
+import { useUser } from '@clerk/nextjs'
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import React, { useState } from 'react'
+import { useGetCallById } from '../../../../../hooks/usegetCallbyId';
+
+const Meeting = ({ params: {id} }: { params: { id: string } }) => {
+  const {user, isLoaded} = useUser();
+  const [isSetupComplete, setisSetupComplete] = useState(false);
+  const {call, isCallLoading} = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading ) return;
+  return (
+    <main className='h-screen w-full'>
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (<MeetingSetup setisSetupComplete={setisSetupComplete} />)
+          : (<MeetingRoom />)}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  )
+}
+
+export default Meeting
